a55ce95ae7d1584fd634bf44722f9a82776f1755
Jingsong Gao <jsgao2109@gmail.com>
2024-02-13 13:52:00 -0800

    Update setup_terminal.sh

--- setup_terminal.sh
+++ setup_terminal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Homebrew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
