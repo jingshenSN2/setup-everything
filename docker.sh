6689dff15f29c85f34aa08cb8c348760a4078daa
Jingsong Gao <jingsongg@nvidia.com>
2024-03-01 14:16:40 -0800

    fix: docker-compose

--- docker.sh
+++ docker.sh
@@ -7,3 +7,7 @@ brew install podman
 # Start Docker(Podman)
 podman machine init
 podman machine start
+
+# Install Docker Compose(Podman Compose)
+echo "Installing Docker Compose(Podman Compose)"
+brew install podman-compose
